61da2d27e3f34c39ecfdaaa9abca3c43a0f7c440
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-18 23:36:01 +0530

    calling security module from dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,3 +4,9 @@ module "vpc" {
     vpc_cidr = "10.0.0.0/16"
     availability_zones = ["us-west-2a", "us-west-2b"]
 }
+
+module "security_group" {
+  source = "../../modules/security-group"
+  environment = "dev"
+  vpc_id = module.vpc.vpc_id
+}
