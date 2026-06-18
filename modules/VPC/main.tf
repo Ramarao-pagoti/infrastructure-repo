9138e53927594e77e5ececb9ef72198b06ed35ff
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-18 19:24:55 +0530

    vpc module creation

---
+++ modules/VPC/main.tf
@@ -0,0 +1,58 @@
+resource "aws_vpc" "main_project_vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "ecommerce-${var.environment}-vpc"
+    }
+  )
+}
+
+resource "aws_subnet" "public" {
+    for_each = local.public_subnets
+    vpc_id            = aws_vpc.main_project_vpc.id
+    cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.subnet_num)
+    availability_zone = each.value.az
+    map_public_ip_on_launch = true
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${var.environment}-${each.key}"
+        Tier = "public"
+      }
+    )
+  
+}
+
+resource "aws_subnet" "private_app" {
+    for_each = local.private_app_subnets
+    vpc_id            = aws_vpc.main_project_vpc.id
+    cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.subnet_num)
+    availability_zone = each.value.az
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${var.environment}-${each.key}"
+        Tier = "app"
+      }
+    )
+  
+}
+
+resource "aws_subnet" "private_db" {
+    for_each = local.private_db_subnets
+    vpc_id            = aws_vpc.main_project_vpc.id
+    cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.subnet_num)
+    availability_zone = each.value.az
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${var.environment}-${each.key}"
+        Tier = "database"
+      }
+    )
+  
+}
