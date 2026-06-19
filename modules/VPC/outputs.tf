ab7a1e45844ae82d5dc311f0fbd056732747e7a3
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-19 17:31:06 +0530

    IAM module json document modified

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
     value = aws_vpc.this.id 
 }
+
+output "private_app_subnet_ids" {
+    value = values(aws_subnet.private_app)[*].id
+}
---
+++ modules/eks/variables.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+type = string  
+}
+variable "cluster_role_arn" {
+    type = string  
+}
+variable "node_role_arn" {
+  type = string
+}
+variable "private_subnet_ids" {
+    type = list(string)  
+}
+variable "eks_node_sg_id" {
+    type = string  
+}
