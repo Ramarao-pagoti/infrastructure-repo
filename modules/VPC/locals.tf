36062d5abd1f704f59f7a04965405cfa79e66d63
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-19 10:25:50 +0530

    project change and new VPC module modified

--- modules/VPC/locals.tf
+++ modules/VPC/locals.tf
@@ -9,27 +9,34 @@ locals {
   public_subnets = {
     public-a = {
       az   = var.availability_zones[0]
+      subnet_number = 1
     }
       public-b = {
           az   = var.availability_zones[1]
+          subnet_number = 2
       }
   }
 
   private_app_subnets = {
     app-a = {
       az   = var.availability_zones[0]
+      subnet_number = 11
     }
     app-b = {
       az   = var.availability_zones[1]
+      subnet_number = 12
     }
   }
 
-  private_db_subnets = {
-    db-a = {
+  private_data_subnets = {
+    data-a = {
       az   = var.availability_zones[0]
+      subnet_number = 21
     }
-    db-b = {
+    data-b = {
       az   = var.availability_zones[1]
+      subnet_number = 22
     }
   }
+
 }
--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "this" {
 resource "aws_subnet" "public" {
     for_each = local.public_subnets
     vpc_id            = aws_vpc.this.id
-    cidr_block        = each.value.cidr
+    cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.subnet_number)
     availability_zone = each.value.az
     map_public_ip_on_launch = true
     tags = merge(
@@ -30,16 +30,29 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private_app" {
     for_each = local.private_app_subnets
     vpc_id            = aws_vpc.this.id
-    cidr_block        = each.value.cidr
+    cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.subnet_number)
     availability_zone = each.value.az
     tags = merge(
       local.common_tags,
       {
         Name = each.key
 
-      "kubernetes.io/role/elb" = "1"
+      "kubernetes.io/role/internal-elb" = "1"
     }
     )
     
   }
 
+resource "aws_subnet" "private_data" {
+    for_each = local.private_data_subnets
+    vpc_id            = aws_vpc.this.id
+    cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.subnet_number)
+    availability_zone = each.value.az
+    tags = merge(
+      local.common_tags,
+      {
+        Name = each.key
+    }
+    )
+    
+  }
