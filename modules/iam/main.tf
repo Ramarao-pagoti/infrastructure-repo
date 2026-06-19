5a3de1b5963c2e50d4195cd401fadd99979e892c
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-20 01:09:25 +0530

    IAM module modified for OIDC implementation

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -111,4 +111,13 @@ resource "aws_iam_role" "alb_controller" {
     assume_role_policy = data.aws_iam_policy_document.alb_controller_assume_role.json
   
 }
-  
+
+resource "aws_iam_policy" "alb_controller" {
+    name = "${var.environment}-alb-controller-policy"
+    policy = file("${path.module}/alb-controller-policy.json")  
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller" {
+    policy_arn = aws_iam_policy.alb_controller
+    role = aws_iam_policy.alb_controller.name
+}
