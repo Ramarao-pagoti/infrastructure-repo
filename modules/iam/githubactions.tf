0a9d86551c9f42f460b6c4a302d51d5fe147f65b
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-24 12:06:08 +0530

    Installing external DNS application

--- modules/iam/githubactions.tf
+++ modules/iam/githubactions.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_openid_connect_provider" "github" {
     url = "https://token.actions.githubusercontent.com"
     client_id_list = [
-        "sts.amazonaws.com
+     "sts.amazonaws.com"
     ]
     thumbprint_list = [
         "6938fd4d98bab03faadb97b34396831e3780aea1"
@@ -80,7 +80,7 @@ resource "aws_iam_policy" "github_ecr_policy" {
 
 resource "aws_iam_role_policy_attachment" "github_ecr" {
 
-  role       = aws_iam_role.github_actions.name
+  role = aws_iam_role.github_actions.name
 
   policy_arn = aws_iam_policy.github_ecr_policy.arn
 }
